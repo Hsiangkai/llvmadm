401cc4f249492fcf1aaaa7dbb20abd944fb80b33
Hsiangkai Wang <kai@Grace.local>
2026-02-27 14:20:08 +0000

    Update to build the latest llvm-project

--- llvmadm.py
+++ llvmadm.py
@@ -1,176 +1,111 @@
-"""Simple script to manage LLVM source tree.
+"""Simple script to manage the LLVM monorepo (llvm-project)."""
 
-"""
-
-import argparse, shutil, os
+import argparse, shutil, os, multiprocessing
 import subprocess
 
-tools = ['clang', 'lld']
-projects = ['compiler-rt', 'test-suite', 'libcxxabi', 'libcxx', 'libunwind']
-llvm_source=os.getcwd()
-cmake_command = 'cmake -DCMAKE_INSTALL_PREFIX={} -DLLVM_TARGETS_TO_BUILD={} -DCMAKE_BUILD_TYPE={} -G "Unix Makefiles" {}'
+llvm_source = os.path.dirname(os.path.abspath(__file__))
 
-llvm_repo = {'clang': 'https://github.com/llvm-mirror/clang.git',
-        'lld': 'https://github.com/llvm-mirror/lld.git',
-        'compiler-rt': 'https://github.com/llvm-mirror/compiler-rt.git',
-        'test-suite': 'https://github.com/llvm-mirror/test-suite.git',
-        'libcxxabi': 'https://github.com/llvm-mirror/libcxxabi.git',
-        'libcxx': 'https://github.com/llvm-mirror/libcxx.git',
-        'libunwind': 'https://github.com/llvm-mirror/libunwind.git'
-        }
-
-def is_lbranch_exist(branch):
-    if branch == 'master':
-        return True
-
-    proc = subprocess.Popen("git branch",
-            stdout=subprocess.PIPE, shell=True, universal_newlines=True)
-    branches = proc.communicate()[0]
-    for lbranch in branches.splitlines():
-        if lbranch[2:] == branch:
-            return True
-
-    return False
-
-def available_rbranch():
-    proc = subprocess.Popen("git branch -a",
-            stdout=subprocess.PIPE, shell=True, universal_newlines=True)
-    branches = proc.communicate()[0]
-
-    avail_branch = []
-    for rbranch in branches.splitlines():
-        if 'origin' in rbranch:
-            branch_pos = rbranch.rfind('/')
-            # skip 'master' branch
-            if rbranch[branch_pos+1:] == 'master':
-                continue
-            avail_branch.append(rbranch[branch_pos+1:])
-
-    return avail_branch
+DEFAULT_PROJECTS = 'clang;lld;mlir'
+DEFAULT_RUNTIMES = 'compiler-rt;libcxx;libcxxabi;libunwind'
 
 
-def create_lbranch(branch):
-    branches = available_rbranch()
-    if not branch in branches:
-        return False
+def update():
+    os.chdir(llvm_source)
+    subprocess.call('git pull --rebase', shell=True)
 
-    subprocess.call("git checkout -b {} origin/{}".format(branch, branch),
-            shell=True)
-    return True
-
-def checkout_lbranch(branch):
-    subprocess.call("git checkout {}".format(branch), shell=True)
-
-def clone_project(project):
-    subprocess.call("git clone {}".format(llvm_repo[project]), shell=True)
-    return
 
 def checkout(branch):
     os.chdir(llvm_source)
-    if not is_lbranch_exist(branch):
-        create_lbranch(branch)
-    else:
-        checkout_lbranch(branch)
+    subprocess.call('git checkout {}'.format(branch), shell=True)
 
-    for tool in tools:
-        tool_dir = os.path.join(llvm_source, 'tools', tool)
-        if not os.path.isdir(tool_dir):
-            os.chdir(os.path.join(llvm_source, 'tools'))
-            clone_project(tool)
 
-        os.chdir(tool_dir)
-        if not is_lbranch_exist(branch):
-            create_lbranch(branch)
-        else:
-            checkout_lbranch(branch)
-
-    for project in projects:
-        project_dir = os.path.join(llvm_source, 'projects', project)
-        if not os.path.isdir(project_dir):
-            os.chdir(os.path.join(llvm_source, 'projects'))
-            clone_project(project)
-
-        os.chdir(project_dir)
-        if not is_lbranch_exist(branch):
-            create_lbranch(branch)
-        else:
-            checkout_lbranch(branch)
-
-def update():
-    # update LLVM source first
-    os.chdir(llvm_source)
-    subprocess.call("git pull --rebase", shell=True)
-
-    # update tools subtree
-    for tool in tools:
-        tool_dir = os.path.join(llvm_source, "tools", tool)
-        if os.path.isdir(tool_dir):
-            os.chdir(tool_dir)
-            subprocess.call("git pull --rebase", shell=True)
-
-    # update projects subtree
-    for project in projects:
-        project_dir = os.path.join(llvm_source, "projects", project)
-        if os.path.isdir(project_dir):
-            os.chdir(project_dir)
-            subprocess.call("git pull --rebase", shell=True)
-
-    return
-
-def build(prefix, buildpath, targets, debug, clean):
+def build(prefix, buildpath, targets, build_type, projects, runtimes, clean, jobs, no_install):
     build_dir = os.path.join(llvm_source, buildpath)
-    build_dir_empty = False
+    need_cmake = False
 
-    if clean:
-        shutil.rmtree(build_dir, ignore_errors=True)
-        os.mkdir(build_dir)
-        build_dir_empty = True
+    if clean and os.path.isdir(build_dir):
+        shutil.rmtree(build_dir)
 
-    if os.path.isdir(build_dir) == False:
-        os.mkdir(build_dir)
-        build_dir_empty = True
+    if not os.path.isdir(build_dir):
+        os.makedirs(build_dir)
+        need_cmake = True
+    elif not os.path.exists(os.path.join(build_dir, 'build.ninja')) and \
+         not os.path.exists(os.path.join(build_dir, 'Makefile')):
+        need_cmake = True
 
-    # The possible values include Debug, Release, RelWithDebInfo, 
-    # and MinSizeRel. Only support Release and Debug now.
-    build_type = 'Release'
-    if debug:
-        build_type = 'Debug'
+    if need_cmake:
+        cmake_args = [
+            'cmake',
+            '-S', os.path.join(llvm_source, 'llvm'),
+            '-B', build_dir,
+            '-DCMAKE_BUILD_TYPE={}'.format(build_type),
+            '-DCMAKE_INSTALL_PREFIX={}'.format(prefix),
+            '-DLLVM_TARGETS_TO_BUILD={}'.format(targets),
+        ]
+        if projects:
+            cmake_args += ['-DLLVM_ENABLE_PROJECTS={}'.format(projects)]
+        if runtimes:
+            cmake_args += ['-DLLVM_ENABLE_RUNTIMES={}'.format(runtimes)]
 
-    os.chdir(build_dir)
-    if build_dir_empty:
-        subprocess.call(cmake_command.format(prefix, targets, build_type, llvm_source), shell=True)
+        if shutil.which('ninja'):
+            cmake_args += ['-G', 'Ninja']
 
-    subprocess.call("make -j4", shell=True)
-    subprocess.call("make install", shell=True)
+        subprocess.call(cmake_args)
 
-    return
+    use_ninja = shutil.which('ninja') and os.path.exists(os.path.join(build_dir, 'build.ninja'))
+
+    if use_ninja:
+        subprocess.call(['ninja', '-C', build_dir, '-j', str(jobs)])
+        if not no_install:
+            subprocess.call(['ninja', '-C', build_dir, 'install'])
+    else:
+        subprocess.call(['make', '-C', build_dir, '-j', str(jobs)])
+        if not no_install:
+            subprocess.call(['make', '-C', build_dir, 'install'])
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Manage the LLVM monorepo')
     sub_parsers = parser.add_subparsers(dest='command', metavar='command')
     sub_parsers.required = True
 
-    sub_parser = sub_parsers.add_parser('update',
-            help='update LLVM from GitHub')
+    sub_parsers.add_parser('update', help='update llvm-project via git pull --rebase')
 
-    sub_parser = sub_parsers.add_parser('checkout',
-            help='switch LLVM branch')
+    sub_parser = sub_parsers.add_parser('checkout', help='switch to a branch')
     sub_parser.add_argument('-b', '--branch', required=True)
 
-    sub_parser = sub_parsers.add_parser('build',
-            help='build LLVM')
-    sub_parser.add_argument('--prefix', default='$HOME/llvm-dev')
-    sub_parser.add_argument('--buildpath', default='build')
-    sub_parser.add_argument('--targets', default='all')
-    sub_parser.add_argument('--debug', action='store_true')
-    sub_parser.add_argument('--clean', action='store_true')
+    sub_parser = sub_parsers.add_parser('build', help='configure and build LLVM')
+    sub_parser.add_argument('--prefix', default=os.path.expanduser('~/llvm-dev'),
+                            help='installation prefix (default: ~/llvm-dev)')
+    sub_parser.add_argument('--buildpath', default='build',
+                            help='build directory relative to repo root (default: build)')
+    sub_parser.add_argument('--targets', default='host',
+                            help='LLVM_TARGETS_TO_BUILD (default: host)')
+    sub_parser.add_argument('--projects', default=DEFAULT_PROJECTS,
+                            help='semicolon-separated LLVM_ENABLE_PROJECTS '
+                                 '(default: {})'.format(DEFAULT_PROJECTS))
+    sub_parser.add_argument('--runtimes', default=DEFAULT_RUNTIMES,
+                            help='semicolon-separated LLVM_ENABLE_RUNTIMES '
+                                 '(default: {})'.format(DEFAULT_RUNTIMES))
+    sub_parser.add_argument('--debug', action='store_true',
+                            help='use Debug build type (default: Release)')
+    sub_parser.add_argument('--clean', action='store_true',
+                            help='wipe the build directory before building')
+    sub_parser.add_argument('--no-install', action='store_true',
+                            help='skip the install step')
+    sub_parser.add_argument('-j', '--jobs', type=int,
+                            default=multiprocessing.cpu_count(),
+                            help='parallel jobs (default: cpu count)')
 
     args = parser.parse_args()
+
     if args.command == 'update':
         update()
     elif args.command == 'checkout':
         checkout(args.branch)
     elif args.command == 'build':
-        build(args.prefix, args.buildpath, args.targets, args.debug, args.clean)
+        build_type = 'Debug' if args.debug else 'Release'
+        build(args.prefix, args.buildpath, args.targets, build_type,
+              args.projects, args.runtimes, args.clean, args.jobs, args.no_install)
     else:
         assert False, 'unexpected command {!r}'.format(args.command)
